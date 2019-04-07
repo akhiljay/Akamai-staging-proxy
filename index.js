fb6039f5e8a682a6a7500d5e8ff0b88f956be2d8
Akhil-mpro-2018 <jpakhil@gmail.com>
2019-04-06 20:31:50 -0400

    added endpoint to push hostname changes to

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ var http = require("http");
 var url = require("url");
 var net = require('net');
 var loki = require('lokijs');
+const bodyParser = require('body-parser');
 
-var db = new loki('hostmap.json');
-var children = db.addCollection('clients')
-children.insert({host:'www.tripadvisor.com', stagingserver: 'www.tripadvisor.com.edgekey-staging.net'})
-children.insert({host:'static.tacdn.com', stagingserver: 'static.tacdn.com.edgekey-staging.net'})
-children.insert({host:'media-cdn.tripadvisor.com', stagingserver: 'media.tacdn.com.edgekey-staging.net'})
-//children.insert({host:'www.akamaidevops.com', stagingserver: 'test.edgekey-staging.net'})
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+var db = new loki('hostmap.db');
+var children = db.addCollection('clients',{indices:['host']},{disableChangesApi:false})
 
 
+var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
 var getHostPortFromString = function (hostString, defaultPort) {
     var host = hostString;
     var port = defaultPort;
@@ -35,53 +36,71 @@ var getHostPortFromString = function (hostString, defaultPort) {
 
 var server = http.createServer(function (req, res) {
 
-  var urlObj = url.parse(req.url);
-  var hostPort = getHostPortFromString(req.url, 80);
-  var stagingdomain = children.findOne( {host:urlObj.host} ) ;
-  if (stagingdomain != null){
-      console.log("not null");
-      console.log(stagingdomain);
-      var target1 = stagingdomain.stagingserver;
-  }
-  else {
-      console.log("is null");
-      console.log(stagingdomain);
-      var target1 = hostPort[0];
-  }
+//if a request comes in for insert then add it to loki db 
+if  (req.url == '/api-staging/insert') {
+   // console.log("matched on path");
+if (req.method == 'POST'){
+    //console.log("matched on POST");
+    var body = "";
+    req.on("data", function (chunk) {
+        body += chunk;
+        //console.log("chunk" + body);
+    });
 
-
-  //var target = urlObj.protocol + "//" + target1;
-  var target = target1;
-  console.log("Proxy HTTP request for:", target1);
-  var proxy = httpProxy.createProxyServer({});
-  proxy.on("error", function (err, req, res) {
-    console.log("proxy error", err);
-    res.end();
-  });
-  proxy.web(req, res, {target: target});
-
-
-}).listen(443);  //this is the port your clients will connect to
-
-
-var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
-
-
-
-
-server.addListener('connect', function (req, socket, bodyhead) {
-    var StagingHost = req.headers;
-    console.log(StagingHost);
-    var hostPort = getHostPortFromString(req.url, 443);
-    var stagingdomain = children.findOne( {host:hostPort[0]} ) ;
+    req.on("end", function(){
+        if (body != null){
+          var parsed = JSON.parse(body);
+          insertintoloki(parsed);
+          //console.log("completed1" + parsed.apiserver);
+          res.writeHead(200, {'Content-Type': 'text/html'})
+          res.end('Field inserted')
+        } 
+    });
+}
+}
+else{
+    var urlObj = url.parse(req.url);
+    var hostPort = getHostPortFromString(req.url, 80);
+    var stagingdomain = children.findOne( {host:urlObj.host} ) ;
     if (stagingdomain != null){
         console.log("not null");
-        console.log(stagingdomain);
-        var hostDomain = stagingdomain.stagingserver;
+        //console.log("sd"+stagingdomain.stagingserver);
+        var target2 = stagingdomain.stagingserver;
+        var target1 = "http://" + target2;
     }
     else {
         console.log("is null");
-        console.log(stagingdomain);
+        var target1 = hostPort[0];
+        //console.log("is nullhost" + target1);
+    }
+   
+    var target = target1;
+    console.log("Proxy HTTP request for:", target);
+    var proxy = httpProxy.createProxyServer({});
+    proxy.on("error", function (err, req, res) {
+      console.log("proxy error", err);
+      res.end();
+    });
+    proxy.web(req, res, {target: target});
+  
+}
+
+}).listen(443);  //this is the port your clients will connect to
+
+server.addListener('connect', function (req, socket, bodyhead) {
+    var StagingHost = req.headers;
+    //console.log(StagingHost);
+    var hostPort = getHostPortFromString(req.url, 443);
+    var stagingdomain = children.findOne( {host:hostPort[0]} ) ;
+    //console.log("lokiDB"+stagingdomain);
+    if (stagingdomain != null){
+        //console.log("not null");
+        //console.log(stagingdomain);
+        var hostDomain = stagingdomain.stagingserver;
+    }
+    else {
+        //console.log("is null");
+        //console.log(stagingdomain);
         var hostDomain = hostPort[0];
     }
     var port = parseInt(hostPort[1]);
@@ -119,3 +138,17 @@ server.addListener('connect', function (req, socket, bodyhead) {
 
 
 });
+
+function insertintoloki(object_body){
+  //insert the values into LokiDB
+  for (var i in object_body.hosts){
+   // console.log("JSON NODe"+i);
+    var apiinserthost = object_body.hosts[i].apihost;
+    var apiinsertserver = object_body.hosts[i].apiserver;
+    //console.log("apiinserthost" + apiinserthost);
+   // console.log("apiinsertserver" +apiinsertserver);
+    children.insert({host:apiinserthost, stagingserver: apiinsertserver});
+  }
+  //console.log(db.serializeChanges(['clients']));
+
+};
